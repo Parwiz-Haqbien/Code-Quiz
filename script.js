4a9e10d9f97ac67632d02402d941e9e03f5d4f3e
Parwiz Haqbien <parwiz.jan03@gmail.com>
2022-09-04 16:04:06 +1000

    Setting the variables

---
+++ script.js
@@ -0,0 +1,10 @@
+var win = document.querySelector('.win');
+var lose = document.querySelector('.lose');
+var timerElement = document.querySelector('.timer-count');
+var startButton = document.querySelector('start-button');
+
+var winCounter = 0;
+var loseCounter = 0;
+var isWin = false;
+var timer;
+var timerCount;
