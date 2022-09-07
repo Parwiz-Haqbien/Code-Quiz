e267481d1588c9cfa5d8d4bcb308ce44b3171ef1
Parwiz Haqbien <parwiz.jan03@gmail.com>
2022-09-07 14:14:03 +1000

    adding  if statement for right and worng answers

--- script.js
+++ script.js
@@ -55,5 +55,48 @@ startGame = () =>{
     questionCounter = 0;
     score = 0;
     availableQuestions = [...question];
-    getNewQuestions()
+    getNewQuestion()
 }
+
+getNewQuestion = () => {
+    if( availableQuestions.length ===0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+        return window.location.assign('./end.html')
+    }
+
+//this is essently saying question 1 of 4 2 of 4 etc
+    questionCounter++,
+    progressText.innerText = 'Questions ${questionCounter} of ${MAX_QESTIONS}';
+    //This bascialy calculate what question we on and corresspond that with the % we are currently at
+    ProgressBarFull.style.width = '${(questioncounter/MAX_QUESTIONS) * 100}%';
+//this is how we are going to calculate the value of the question
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+    //this will keep track of what question we are on 
+    currentQuestion =availableQuestions[questionsIndex];
+    //this will know what question will be asked
+    question.innerText = currentQuestion.question;
+
+    choices.forEach(choice => {
+        //this will know what choice we are clicking on 
+        const number= choice.dataset ['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    })
+
+    availableQuestions.splice (questionsIndex, 1);
+
+    accesptingAnswers = true;
+}
+
+choices.forEach( choice => {
+choice.addEventListener ('click', e => {
+        if (!accesptingAnswers) return;
+        
+        accesptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice. dataset ['number'];
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+        
+})
+})
+
