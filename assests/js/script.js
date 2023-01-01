20b6f495c4d75082ddd233a8e5c39d99e491650f
Parwiz-Haqbien <Prwiz.jan03@gmail.com>
2023-01-01 20:14:57 +1100

    grabbing the html ids to work on in the js

--- assests/js/script.js
+++ assests/js/script.js
@@ -2,3 +2,27 @@ var questNumber = 0;
 var finalScore = 0;
 var timeLeft = 75;
 var countDown;
+
+const timeEl = document.querySelector(".timer");
+const content = document.querySelector(".container");
+const questionEl = document.querySelector(".question");
+const answersEl = document.querySelector(".info");
+const startPage = document.getElementById("introPart");
+const p5 = document.getElementById("endPart");
+const score = document.getElementById("score");
+var result = document.querySelector(".result");
+var playerName = document.getElementById("initials");
+var nameList = document.getElementById("names");
+
+
+const submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", nextPage);
+
+const clearButton = document.getElementById("clearScore");
+clearButton.addEventListener("click", clearScore);
+
+const backButton = document.getElementById("goBack");
+backButton.addEventListener("click", goBack);
+
+const viewScores = document.getElementById("viewHighScores");
+viewScores.addEventListener("click", displayHighScores);
